0b1901cb747c0c78a5545917a55f97276ca26e90
Yann Obiang <71565394+yannobiang@users.noreply.github.com>
2022-11-16 11:20:52 +0100

    Update cosine_distance.py

--- cosine_distance.py
+++ cosine_distance.py
@@ -10,3 +10,7 @@ cos(u.mean(axis=1), v.mean(axis=1))
 ## calcul de distance entre les deux tensor
 ## bert_distance = distance(np.array(word1.embedding),
 ##                            np.array(word2.embedding))
+
+
+## git add remote repository:
+## git remote set-url origin https://github.com/yannobiang/api_otteo.git
